be0cd4a849dd2f219c8849dda87050a8647f7dde
stanrothstegge <stanrothstegge@gmail.com>
2019-04-20 11:17:16 +0200

    Javadoc, Design of QuestionScreen

---
+++ app/src/main/java/model/ChoicePart.java
@@ -0,0 +1,6 @@
+package model;
+
+public class ChoicePart {
+    String text;
+    int position;
+}
--- app/src/main/java/oranjeblauwgeel/stanrothstegge/minorgeschiedenis/Overhoorscherm.java
+++ app/src/main/java/oranjeblauwgeel/stanrothstegge/minorgeschiedenis/Overhoorscherm.java
@@ -1,42 +1,42 @@
 package oranjeblauwgeel.stanrothstegge.minorgeschiedenis;
 
-import android.annotation.SuppressLint;
 import android.graphics.Color;
-import android.graphics.drawable.Drawable;
 import android.graphics.drawable.GradientDrawable;
-import android.os.Build;
 import android.os.Bundle;
-
-import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
-import android.widget.Toast;
-
 import com.google.android.flexbox.FlexboxLayout;
 
 public class Overhoorscherm extends AppCompatActivity {
-    TextView question;
-    FlexboxLayout possibleAnswers;
-    FlexboxLayout currentGivenAnswer;
-
-    int background;
-
-
-
+    TextView question; //De vraag die beantwoord moet worden.
+    FlexboxLayout possibleAnswers; //De mogelijke antwoorden op de vraag.
+    FlexboxLayout currentGivenAnswer; //Het huidige geselecteerde antwoord.
+    RelativeLayout layout;
+    Button menu;
+    Button proceed;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_overhoorscherm);
-
+        //init elements
         question = findViewById(R.id.questionTextview);
         currentGivenAnswer = findViewById(R.id.flexbox);
         possibleAnswers = findViewById(R.id.flexboxMultipleChoice);
+        layout = findViewById(R.id.questionlayout);
+        menu = findViewById(R.id.menuButton);
+        proceed = findViewById(R.id.proceedButton);
 
+        GradientDrawable menuStyling = (GradientDrawable) getDrawable(R.drawable.menu_shape);
+        menu.setBackground(menuStyling);
+        proceed.setBackground(menuStyling);
+            GradientDrawable questionBack = (GradientDrawable) getDrawable(R.drawable.question_shape);
+        layout.setBackground(questionBack);
+        question.setTextColor(Color.WHITE);
 
+        //TODO vragen importeren vanuit een csv
         question.setText("In welk land was de staatsvorm na de Eerste Wereldoorlog nog hetzelfde als voor die oorlog?\n" +
                 "\n");
 
@@ -50,38 +50,47 @@ public class Overhoorscherm extends AppCompatActivity {
 
 
 
+
     }
 
-
-    private void VoegAntwoordToe(final String naam, int kleur) {
-        final Button txt1 = new Button(getApplicationContext());
-
+    /**
+     *
+     * @param antwoordGedeelte de tekst van de meerkeuze optie.
+     * @param kleur kleur van de button.
+     */
+    private void VoegAntwoordToe(final String antwoordGedeelte, int kleur) {
+        final Button antwoordOptieButton = new Button(getApplicationContext());
+        //Drawable wordt gebruikt voor de vorm van de button.
         GradientDrawable background = (GradientDrawable) getDrawable(R.drawable.my_shape);
+        //Buttons hebben verschillende kleuren.
         background.setColor(kleur);
-
-        txt1.setBackground(background);
-        txt1.setText(naam);
-        txt1.setTextColor(Color.WHITE);
-
-        txt1.setOnClickListener(new View.OnClickListener() {
+        //Set vorm van button
+        antwoordOptieButton.setBackground(background);
+        //Geef het antwoord weer
+        antwoordOptieButton.setText(antwoordGedeelte);
+        antwoordOptieButton.setTextColor(Color.WHITE);
+        /**
+         * Implementeer On-Click
+         * 1.Gebruiker selecteert een antwoord uit het Multi-Choice venster:
+         * -> Swap button naar het 'Huidige Antwoord' venster.
+         * 2.Gebruiker Anuleert antwoord:
+         * -> Button wordt terug gezet in Multi-Choice venster.
+         */
+        antwoordOptieButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(v.getParent() == currentGivenAnswer){
-                    currentGivenAnswer.removeView(txt1);
-                    possibleAnswers.addView(txt1);
-                } else if(v.getParent() == possibleAnswers){
-                    possibleAnswers.removeView(txt1);
-                    currentGivenAnswer.addView(txt1);
+                if(v.getParent() == currentGivenAnswer){                    //Remove answer
+                    currentGivenAnswer.removeView(antwoordOptieButton);
+                    possibleAnswers.addView(antwoordOptieButton);
+                } else if(v.getParent() == possibleAnswers){                //Add answer
+                    possibleAnswers.removeView(antwoordOptieButton);
+                    currentGivenAnswer.addView(antwoordOptieButton);
                 }
 
             }
         });
-      // container.addView(txt1);
-        possibleAnswers.addView(txt1);
-        FlexboxLayout.LayoutParams layoutParams = (FlexboxLayout.LayoutParams) txt1.getLayoutParams();
-        layoutParams.width = LinearLayout.LayoutParams.WRAP_CONTENT;
-
-        txt1.setLayoutParams(layoutParams);
+        //Voeg button toe aan Flexbox
+        possibleAnswers.addView(antwoordOptieButton);
     }
 
 
